463679ddf454a17aa1871eed311fb04dff75168c
Tyler Tivadar <tylertivadar@MacBook-Pro.local>
2021-02-20 23:59:23 -0800

    added default list value

--- h4h21/api/models.py
+++ h4h21/api/models.py
@@ -8,6 +8,6 @@ class FoodItem(models.Model):
     time_posted = models.DateTimeField(auto_now_add=True)
     time_available_start = models.DateTimeField(auto_now=False, auto_now_add=False)
     time_available_end = models.DateTimeField(auto_now=False, auto_now_add=False)
-    food_category = ArrayField(base_field=models.CharField(max_length=15, unique=False, blank=True), size=5)
+    food_category = ArrayField(base_field=models.CharField(max_length=15, unique=False, blank=True), size=5, default=list)
     food_desc = models.CharField(max_length=50)
     
